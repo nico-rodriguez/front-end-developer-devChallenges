5fb40dd227e7676be973671ef0d134c83d15174a
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-05-09 17:34:47 -0300

    Set footer link to repository

--- windbnb/src/components/Footer/index.js
+++ windbnb/src/components/Footer/index.js
@@ -4,7 +4,10 @@ export default function Footer({ isInactive }) {
   return (
     <footer className={'footer' + (isInactive ? ' footer--inactive' : '')}>
       created by{' '}
-      <a className='footer__link' href='https://github.com/nico-rodriguez'>
+      <a
+        className='footer__link'
+        href='https://github.com/nico-rodriguez/front-end-developer-devChallenges/tree/master/windbnb'
+      >
         nico-rodriguez
       </a>{' '}
       -{' '}
