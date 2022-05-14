5e4a497f5b71d229df4fa7584a11a9f37a5e0fbc
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-05-13 22:07:58 -0300

    Complete README.md

--- todo-app/src/components/Footer/index.tsx
+++ todo-app/src/components/Footer/index.tsx
@@ -4,11 +4,21 @@ export default function Footer() {
   return (
     <footer className='footer'>
       created by{' '}
-      <a href='#' className='footer__link'>
+      <a
+        href='https://github.com/nico-rodriguez/front-end-developer-devChallenges/tree/master/todo-app'
+        className='footer__link'
+        target='_blank'
+        rel='noreferrer'
+      >
         nico-rodriguez
       </a>{' '}
       -{' '}
-      <a href='https://devchallenges.io/' className='footer__link'>
+      <a
+        href='https://devchallenges.io/'
+        className='footer__link'
+        target='_blank'
+        rel='noreferrer'
+      >
         devChallenges.io
       </a>
     </footer>
