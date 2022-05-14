fe6b74b4c94ffb0f9008bbacc0d0ebecd0408ce7
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-05-14 00:53:01 -0300

    Add header

--- github-jobs/src/App.tsx
+++ github-jobs/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return <div className='wrapper'></div>;
+  return (
+    <div className='wrapper'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ github-jobs/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <h1 className='header__text'>
+        <strong>GitHub</strong> Jobs
+      </h1>
+    </header>
+  );
+}
