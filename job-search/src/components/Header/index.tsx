ad5a0fe42e8c2b9a8689ba84609d669b3585de58
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-05-15 16:53:14 -0300

    Change app title

--- job-search/src/components/Header/index.tsx
+++ job-search/src/components/Header/index.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className='header'>
       <h1 className='header__text'>
-        <strong>GitHub</strong> Jobs
+        <strong>Job</strong> Search
       </h1>
     </header>
   );
