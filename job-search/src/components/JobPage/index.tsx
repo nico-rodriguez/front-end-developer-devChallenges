4aa1fa45c37397152e6e323df0088f48414497fa
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-05-17 20:19:59 -0300

    Scroll to top when viewing a job

--- job-search/src/components/JobPage/index.tsx
+++ job-search/src/components/JobPage/index.tsx
@@ -4,10 +4,16 @@ import { getJobById } from '../../services/job';
 import './Job.css';
 import JobAside from './JobAside';
 import JobContent from './JobContent';
+import { useEffect } from 'react';
 
 export default function JobPage() {
   const { jobId } = useParams();
   const job = getJobById(jobId as string);
+
+  // Scrol page to top when viewing a job
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <section className='job-page'>
