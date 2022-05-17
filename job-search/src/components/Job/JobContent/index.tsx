963c3f67082f05ccb225e4d58edcc93eddfd3357
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-05-17 19:51:59 -0300

    Add fonts and layout for JobContent

--- job-search/src/components/Job/JobContent/index.tsx
+++ job-search/src/components/Job/JobContent/index.tsx
@@ -9,13 +9,19 @@ export default function JobContent({ job }: JobContentProps) {
   return (
     <main className='job-page__information'>
       <article>
-        <h2 className='job__title'>{job.title}</h2>
-        <span className='job__type'>{job.type}</span>
-        <time className='job__date' dateTime={job.created_at}>
-          &#xe8b5; {new Date(Date.parse(job.created_at)).toDateString()}
-        </time>
-        <h3 className='job__company'>{job.company}</h3>
-        <span className='job__location'>&#xe80b; {job.location}</span>
+        <div className='position-information'>
+          <div className='position-information__main'>
+            <h2 className='job__title'>{job.title}</h2>
+            <span className='job__type'>{job.type}</span>
+          </div>
+          <time className='job__date' dateTime={job.created_at}>
+            &#xe8b5; {new Date(Date.parse(job.created_at)).toDateString()}
+          </time>
+        </div>
+        <div className='company-information'>
+          <h3 className='job__company'>{job.company}</h3>
+          <span className='job__location'>&#xe80b; {job.location}</span>
+        </div>
         <div
           className='job__description'
           dangerouslySetInnerHTML={{ __html: job.description }}
